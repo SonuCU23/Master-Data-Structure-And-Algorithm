f4e8db49398bb2f77faa531d1f7e176e0edae02e
Sonu Ray <sonuray1604@gmail.com>
2024-03-08 20:57:26 +0530

    Day 3 : Problem on Arrays - 2

---
+++ arrays/CountElementsFrequency.java
@@ -0,0 +1,60 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+    Count Elements With Maximum Frequency : https://leetcode.com/problems/count-elements-with-maximum-frequency/?envType=daily-question&envId=2024-03-08
+
+    You are given an array nums consisting of positive integers. Return the total frequencies of elements in nums such that
+    those elements all have the maximum frequency. The frequency of an element is the number of occurrences of that element
+    in the array.
+
+    Example 1:
+
+    Input: nums = [1,2,2,3,1,4]
+    Output: 4
+    Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+    So the number of elements in the array with maximum frequency is 4.
+*
+* */
+
+public class CountElementsFrequency {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int count = countElementFrequency(arr, n);
+        System.out.println(count);
+    }
+
+    public  static int countElementFrequency(int[] arr, int n){
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFrequency = 0;
+
+        for (int frequency : frequencyMap.values()) {
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+            }
+        }
+
+        int count = 0;
+        for (int frequency : frequencyMap.values()) {
+            if (frequency == maxFrequency) {
+                count += maxFrequency;
+            }
+        }
+
+
+        return count;
+    }
+}
---
+++ arrays/LeadersArray.java
@@ -0,0 +1,49 @@
+package arrays;
+
+/*
+* Leaders in an array : https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1
+* Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is a leader if
+  it is greater than or equal to all the elements to its right side. The rightmost element is always a leader.
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+
+* */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class LeadersArray {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        ArrayList<Integer> res = leadersInAnArray(arr);
+        for (Integer element : res) {
+            System.out.print(element + " ");
+        }
+    }
+
+    public static ArrayList<Integer> leadersInAnArray(int[] arr){
+        ArrayList<Integer> res = new ArrayList<>();
+
+        res.add(arr[arr.length - 1]);
+        for(int i = arr.length - 2; i >=0; i--){
+            if(arr[i] >= res.get(res.size() - 1))
+                res.add(arr[i]);
+        }
+
+        Collections.reverse(res);
+
+        return res;
+    }
+}
